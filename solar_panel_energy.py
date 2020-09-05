13d9ef95bdb86cf7e9e137944d5fd501744e747d
NWiktor <31969508+NWiktor@users.noreply.github.com>
2020-09-05 10:32:47 +0200

    init upload

---
+++ solar_panel_energy.py
@@ -0,0 +1,309 @@
+# -*- coding: utf-8 -*-
+import sys
+import math
+import sun_position as SP
+
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+from plotly.graph_objs import Scatter, Figure, Layout
+
+
+def sun_energy(alt, azimut, delta_sec, panel_alt, panel_azimut):
+    panel_area = 1.64 * 0.992  # m2
+    efficiency = 1  # arbitrary
+
+    azimut_diff = panel_azimut - azimut
+    panel_diff = 90 - panel_alt - alt
+
+    if alt > 0.5:  # Ha a Nap teljes terjedelmével a horizont felett van
+        if abs(azimut_diff) < 90:  # Ha a napsugár és a panel szögkülönbsége kisebb mint 90°
+            # intensity in kw/m2 = 1.35 * (1.00/1.35)^sec(angle of sun from zenith)
+            i = 1.35 * math.pow((1.00/1.35), (1 / math.cos(math.radians(90-alt))))
+            a = abs(math.cos(math.radians(panel_diff)))
+            b = abs(math.cos(math.radians(azimut_diff)))
+
+            energy = panel_area * delta_sec * a * b * i * efficiency  # kW/m2 * s * m2
+        else:
+            energy = 0
+    else:
+        energy = 0
+
+    return energy
+
+
+def energy_DAT(alt, azimut, delta_sec):
+    global panel_power
+
+    if alt > 0.5:  # Ha a Nap teljes terjedelmével a horizont felett van
+        # intensity in kw/m2 = 1.35 * (1.00/1.35)^sec(angle of sun from zenith)
+        i = 1.35 * math.pow((1.00/1.35), (1 / math.cos(math.radians(90-alt))))
+        energy = (panel_power * delta_sec) * i / 1000  # kJ
+    else:
+        energy = 0
+
+    return energy
+
+
+def energy_fixed(alt, azimut, delta_sec, panel_alt, panel_azimut):
+    global panel_power
+
+    azimut_diff = panel_azimut - azimut
+    panel_diff = (90 - panel_alt) - alt
+
+    if alt > 0.5:  # Ha a Nap teljes terjedelmével a horizont felett van
+        if abs(azimut_diff) < 90:  # Ha a napsugár és a panel szögkülönbsége kisebb mint 90°
+            # intensity in kw/m2 = 1.35 * (1.00/1.35)^sec(angle of sun from zenith)
+            i = 1.35 * math.pow((1.00/1.35), (1 / math.cos(math.radians(90-alt))))
+            # a = math.cos(math.radians(panel_diff))
+            # b = math.cos(math.radians(azimut_diff))
+
+            r = 1
+            # Sun coordinates
+            x1 = r * math.cos(math.radians(alt)) * math.cos(math.radians(azimut))
+            y1 = r * math.cos(math.radians(alt)) * math.sin(math.radians(azimut))
+            z1 = r * math.sin(math.radians(alt))
+
+            # Panel normal vector
+            x2 = r * math.cos(math.radians(90 - panel_alt)) * math.cos(math.radians(panel_azimut))
+            y2 = r * math.cos(math.radians(90 - panel_alt)) * math.sin(math.radians(panel_azimut))
+            z2 = r * math.sin(math.radians(90 - panel_alt))
+
+            aa = (x1 * x2) + (y1 * y2) + (z1 * z2)
+            bb = math.sqrt(math.pow(x1, 2) + math.pow(y1, 2) + math.pow(z1, 2)) * math.sqrt(math.pow(x2, 2) + math.pow(y2, 2) + math.pow(z2, 2))
+            cos_diff_angle = float(aa/bb)
+
+            energy = panel_power * delta_sec * cos_diff_angle * i / 1000  # kJ
+        else:
+            energy = 0
+    else:
+        energy = 0
+
+    return energy
+
+
+def sun_daily_movement(date, resolution):
+    date_s = date
+
+    step_fraction = 0.00000000000
+    steps = 24*60/resolution  # stepek száma egy napon (min)
+    step_fraction = float(24/24) / float(steps)  # day / steps costant
+
+    s = 0
+    time_0_data = []
+    elev_data = []
+    alt_data = []
+
+    date_0 = float(date_s)
+
+    for s in range(0, steps):  # Sun position data
+        UT_time_0, elev_0, azim_0 = SP.sun_position(date_0, False)
+        time_0_data.append(UT_time_0)
+        elev_data.append(elev_0)
+        alt_data.append(azim_0)
+
+        s += 1
+        date_0 = float(date_0) + float(step_fraction)
+
+    plot([Scatter(x=time_0_data, y=elev_data, name="Elevation"), Scatter(x=time_0_data, y=alt_data, name="Azimuth")])
+
+
+def daily_energy_gain(date, resolution, print_plot):  # 38.82 fok dőlésszög esetén
+    date_s = date
+    panel_tilt = 38.82
+
+    step_fraction = 0.00000000000
+    steps = 24*60/resolution  # stepek száma egy napon (min)
+    step_fraction = float(24/24) / float(steps)  # day / steps costant
+
+    delta_T = resolution*60  # (s) lépésköz
+    energy_DAT_data = 0
+    energy_fix_data = 0
+    sum_energy_DAT_data = 0
+    sum_energy_fix_data = 0
+
+    time_data = []
+    DAT_data = []
+    fix_data = []
+
+    date_0 = float(date_s)
+
+    for k in range(0, steps + 1):
+        UT_time, elev, azim = SP.sun_position(date_0, False)
+
+        # Energia adatok
+        energy_DAT_data = energy_DAT(elev, azim, delta_T)  # kJ
+        sum_energy_DAT_data += energy_DAT_data  # kJ
+
+        energy_fix_data = energy_fixed(elev, azim, delta_T, panel_tilt, 180)  # kJ
+        sum_energy_fix_data += energy_fix_data  # kJ
+
+        time_data.append(UT_time)
+        DAT_data.append(energy_DAT_data)
+        fix_data.append(energy_fix_data)
+
+        date_0 = float(date_0) + float(step_fraction)
+
+    if print_plot is True:
+        print("Kinyert összenergia (DAT): " + str(sum_energy_DAT_data) + " kJ")
+        print("Kinyert összenergia (fixed at " + str(panel_tilt) + " deg): " + str(sum_energy_fix_data) + " kJ")
+
+        graph_name = str("FIX" + str(panel_tilt))
+        plot([Scatter(x=time_data, y=DAT_data, name="DAT"), Scatter(x=time_data, y=fix_data, name=graph_name)])
+    else:
+        pass
+
+    return sum_energy_DAT_data, sum_energy_fix_data, panel_tilt
+
+
+def daily_energy_gain_fixed(date, resolution, panel_tilt, print_plot):
+    date_s = date
+
+    step_fraction = 0.00000000000
+    steps = 24*60/resolution  # stepek száma egy napon (min)
+    step_fraction = float(24/24) / float(steps)  # day / steps costant
+
+    delta_T = resolution*60  # (s) lépésköz
+    energy_fix_data = 0
+    sum_energy_fix_data = 0
+
+    time_data = []
+    fix_data = []
+
+    date_0 = float(date_s)
+
+    for k in range(0, steps + 1):
+        UT_time, elev, azim = SP.sun_position(date_0, False)
+
+        # Energia adatok
+        energy_fix_data = energy_fixed(elev, azim, delta_T, panel_tilt, 180)  # kJ
+        sum_energy_fix_data += energy_fix_data  # kJ
+
+        time_data.append(UT_time)
+        fix_data.append(energy_fix_data)
+
+        date_0 = float(date_0) + float(step_fraction)
+
+    if print_plot is True:
+        print("Kinyert összenergia (fixed at " + str(panel_tilt) + " deg): " + str(sum_energy_fix_data) + " kJ")
+        graph_name = str("FIX" + str(panel_tilt))
+        plot([Scatter(x=time_data, y=fix_data, name=graph_name)])
+    else:
+        pass
+
+    return sum_energy_fix_data
+
+
+def interval_energy_gain(date, number_of_days):
+    date_s = date
+
+    sum_energy_DAT_data = 0
+    sum_energy_fix_data = 0
+
+    time_data = []
+    DAT_data = []
+    fix_data = []
+
+    date_0 = date_s
+    print("Iteration started!")
+
+    for m in range(0, number_of_days + 1):
+        DAT_energy, FIX_energy, panel_tilt = daily_energy_gain(date_0, 1, False)
+
+        sum_energy_DAT_data += DAT_energy  # kJ
+        sum_energy_fix_data += FIX_energy  # kJ
+
+        time_data.append(m)
+        DAT_data.append(DAT_energy)
+        fix_data.append(FIX_energy)
+
+        date_0 += 1
+
+    print("Kinyert összenergia (DAT): " + str(sum_energy_DAT_data) + " kJ")
+    print("Kinyert összenergia (fixed at " + str(panel_tilt) + " deg): " + str(sum_energy_fix_data) + " kJ")
+    graph_name = str("FIX" + str(panel_tilt))
+    plot([Scatter(x=time_data, y=DAT_data, name="DAT"), Scatter(x=time_data, y=fix_data, name=graph_name)])
+
+
+def interval_energy_gain_optimum_calc(date, number_of_days, start_angle, end_angle, count):  # expected optimum angle: 38.82
+    date_s = date
+    angle_step = float(end_angle - start_angle)/float(count)
+
+    angle_1 = 20
+    angle_2 = 40
+    angle_3 = 60
+    day_data_1 = []
+    day_data_2 = []
+    day_data_3 = []
+    daily_energy_data_1 = []
+    daily_energy_data_2 = []
+    daily_energy_data_3 = []
+
+    sum_energy_fix_data = 0
+    angle_data = []
+    energy_data = []
+
+    max_energy = 0
+    optimal_tilt_angle = 0
+
+    date_0 = date_s
+    angle_0 = start_angle
+    print("Iteration started!")
+
+    for o in range(0, count + 1):
+        for m in range(0, number_of_days + 1):
+            FIX_energy = daily_energy_gain_fixed(date_0, 1, angle_0, False)
+            sum_energy_fix_data += FIX_energy  # kJ
+
+            if angle_0 == angle_1:
+                day_data_1.append(m)
+                daily_energy_data_1.append(FIX_energy)
+            elif angle_0 == angle_2:
+                day_data_2.append(m)
+                daily_energy_data_2.append(FIX_energy)
+            elif angle_0 == angle_3:
+                day_data_3.append(m)
+                daily_energy_data_3.append(FIX_energy)
+            else:
+                pass
+
+            date_0 += 1
+
+        angle_data.append(angle_0)
+        energy_data.append(sum_energy_fix_data)
+
+        if sum_energy_fix_data > max_energy:
+            max_energy = sum_energy_fix_data
+            optimal_tilt_angle = angle_0
+        else:
+            pass
+
+        sum_energy_fix_data = 0
+        print("Iteration " + str(o + 1) + "/" + str(count + 1) + " is ready!" + " Angle: " + str(angle_0) + "°.")
+        date_0 = date_s
+        angle_0 += angle_step
+
+    efficiency_data = []
+    for p in range(0, len(energy_data)):
+        efficiency_data.append(energy_data[p]/max_energy)
+
+    print("Max. energy yield with the optimal tilt angle (" + str(optimal_tilt_angle) + "): " + str(max_energy/1000/1000) + " GJ")
+    # plot([Scatter(x=angle_data, y=energy_data, name="Fix_angle")])
+    plot([Scatter(x=angle_data, y=efficiency_data, name="Fix_angle")])
+
+    graph_name_1 = "FIX" + str(angle_1)
+    graph_name_2 = "FIX" + str(angle_2)
+    graph_name_3 = "FIX" + str(angle_3)
+    # plot([Scatter(x=day_data_1, y=daily_energy_data_1, name=graph_name_1), Scatter(x=day_data_2, y=daily_energy_data_2, name=graph_name_2), Scatter(x=day_data_3, y=daily_energy_data_3, name=graph_name_3)])
+
+
+# -- MAIN --
+n_panel = 1760  # db
+panel_power = 285  # W
+
+# sun_daily_movement(SP.JD(2018, 2, 14, 0, 0, False), 1)
+# daily_energy_gain(SP.JD(2018, 2, 14, 0, 0, False), 1, True)
+# interval_energy_gain(SP.JD(2018, 2, 14, 0, 0, False), 365)
+
+# daily_energy_gain_fixed(SP.JD(2018, 2, 14, 0, 0, False), 1, 40, True)
+# daily_energy_gain_fixed(SP.JD(2018, 2, 14, 0, 0, False), 1, 45, True)
+# daily_energy_gain_fixed(SP.JD(2018, 2, 14, 0, 0, False), 1, 50, True)
+interval_energy_gain_optimum_calc(SP.JD(2018, 1, 1, 0, 0, False), 365, 15, 50, 35)
---
+++ sun_position.py
@@ -0,0 +1,161 @@
+# -*- coding: utf-8 -*-
+import sys
+import math
+
+
+def hour_to_time(time):  # tizedesjegyben kifejezve, ez kell?
+    time_h = int(math.floor(time))
+    time_m = int(math.floor((time-time_h)*60))
+    time_s = int(math.floor((((time-time_h)*60)-time_m)*60))  # másodperc 2-ik tizedesjegyéig pontos
+    return time_h, time_m, time_s
+
+
+def day_to_time(time):  # tizedesjegyben kifejezve, ez kell?
+    time_d = int(math.floor(time))
+    time_h = int(math.floor((time-time_d)*24))
+    time_m = int(math.floor((((time-time_d)*24)-time_h)*60))  # másodperc 2-ik tizedesjegyéig pontos
+    time_s = int(math.floor((((((time-time_d)*24)-time_h)*60)-time_m)*60))
+    return time_d, time_h, time_m, time_s
+
+
+def JD(year, month, day, hour, minute, info):
+    Y = year
+    M = month
+    D = day + float(hour)/24 + float(minute)/1440
+
+    if M == 1 or M == 2:
+        Y -= 1
+        M += 12
+
+    else:
+        pass
+
+    A = int(Y/100)
+
+    x = Y*10000 + M*100 + D
+    if x < 15821015:  # However, if the date is earlier than October 15, 1582 then B=0.
+        B = 0
+    else:
+        B = 2-A+int(A/4)
+
+    if Y < 0:
+        C = int((365.25*Y)-0.75)
+    else:
+        C = int(365.25*Y)
+
+    E = int(30.6001*(M+1))
+    JD = B + C + D + E + 1720994.5
+
+    if info is True:
+        date_string = str(year) + "-" + str(month) + "-" + str(day) + " " + str(hour) + ":" + str(minute)
+        print(date_string + " julián dátuma (JD): " + str(JD) + "\n")
+
+    else:
+        pass
+
+    return JD
+
+
+def sun_position(julian_date, info):
+    global latitude
+    global longitude
+    global timezone
+
+    # print(str(julian_date))
+    julian_date -= timezone/24  # JD helyi idő szerint definiálandó!
+    # print(str(julian_date))
+
+    n = julian_date - 2451545.0000000
+    L = 280.460 + 0.9856474*n  # fok
+    g = 357.528 + 0.9856003*n  # fok
+
+    k = math.floor(L/360)
+    m = math.floor(g/360)
+
+    L = math.radians(L - k*360)  # radián
+    g = math.radians(g - m*360)  # radián
+
+    # Ekliptikus koordinátarendszer
+    sun_lambda = L + math.radians(1.915)*math.sin(g) + math.radians(0.020)*math.sin(2*g)  # radián
+    sun_beta = 0  # radián
+    sun_distance = 1.00014 - 0.01671*math.cos(g) - 0.00014*math.cos(2*g)  # AU
+
+    # Áttérés ekvatoriális koorináta rendszerre
+    epsilon = math.radians(23.4393 - 0.0000004*n)  # 23.43699 fok - pillanatnyi érték, 22.1 és 24.5 között váltakozik 41000 éves ciklusban (radiánban kifejezve)
+    RA = math.atan2(math.cos(epsilon)*math.sin(sun_lambda), math.cos(sun_lambda))  # radián
+    DEC = math.asin(math.sin(epsilon)*math.sin(sun_lambda))  # radián
+
+    # LST számítása
+    JD_null = round(julian_date) - 0.5
+    H = (julian_date - JD_null)*24
+    D_null = JD_null - 2451545.0
+    T = n/36525
+    GST = 6.697374558 + 0.06570982441908*D_null + 1.00273790935*H + 0.000026*(T*T)  # órában
+    o = math.floor(GST/24)
+    GST = GST - o*24  # óra
+
+    LST = GST + (longitude/15)
+    p = math.floor(LST/24)
+    LST = LST - p*24  # óra
+
+    GST_h, GST_m, GST_s = hour_to_time(GST)
+    LST_h, LST_m, LST_s = hour_to_time(LST)
+
+    day_fraction = julian_date-int(math.floor(julian_date)) + 0.50000000
+    UT_d, UT_h, UT_m, UT_s = day_to_time(day_fraction)
+
+    # Áttérés horizontális koordinátarendszerbe
+    h = math.radians(LST*15) - RA  # radián
+    sun_elevation = math.asin(math.sin(latitude)*math.sin(DEC) + math.cos(latitude)*math.cos(DEC)*math.cos(h))
+
+    a = -1*math.sin(latitude)*math.cos(DEC)*math.cos(h) + math.cos(latitude)*math.sin(DEC)
+    b = math.cos(DEC)*math.sin(h)
+    # sun_azimut = -1*math.atan2( -1*math.sin(latitude)*math.cos(DEC)*math.cos(h) + math.cos(latitude)*math.sin(DEC) , math.cos(DEC)*math.sin(h) )
+    sun_azimut = -1*math.atan2(b, a)
+
+    if sun_azimut < 0:
+        sun_azimut = sun_azimut + 2*math.pi
+    else:
+        pass
+
+    # LST_string = str(LST_h) + ":" + str(LST_m) + ":" + str(LST_s)
+    UT_string = str(UT_h) + ":" + str(UT_m) + ":" + str(UT_s)
+    sun_elevation_deg = math.degrees(sun_elevation)
+    sun_azimut_deg = math.degrees(sun_azimut)
+
+    if info is True:
+        print("-- A " + str(julian_date) + " (JD) napon, a Nap ekliptikus koordinátái: --\n"
+        # + "Number of days from epoch (n): " + str(n) +" d \n"
+        # + "Mean longitude (L): " + str(math.degrees(L)) +"° \n"
+        # + "Mean anomaly (g): " + str(math.degrees(g)) + "° \n"
+        + "Ecliptic longitude (lambda): " + str(math.degrees(sun_lambda)) + "°\n"
+        + "Ecliptic latitude (beta): " + str(math.degrees(sun_beta)) + "°\n"
+        + "Earth-Sun distance (R): " + str(sun_distance) + " AU\n" + "\n"
+
+        "-- A Nap ekvatoriális koordinátái: --\n"
+        + "Right ascension (RA): " + str(math.degrees(RA)) + "°\n"
+        + "Declination (DEC): " + str(math.degrees(DEC)) + "°\n" + "\n"
+
+        "-- A Nap horizontális koordinátái: --\n"
+        + "Magasság (a): " + str(math.degrees(sun_elevation)) + "°\n"
+        + "Azimut (A): " + str(math.degrees(sun_azimut_deg)) + "°\n" + "\n"
+
+        "-- További időadatok: --\n"
+        + "Universal Time (UT): " + str(UT_h) + ":" + str(UT_m) + ":" + str(UT_s) + "\n"
+        + "Greenwich Sidereal Time (GST): " + str(GST_h) + ":" + str(GST_m) + ":" + str(GST_s) + "\n"
+        + "Local Sidereal Time (LST): " + str(LST_h) + ":" + str(LST_m) + ":" + str(LST_s) + "\n" + "\n"
+        )
+
+    else:
+        pass
+
+    return UT_string, sun_elevation_deg, sun_azimut_deg
+
+
+latitude = math.radians(47.497912)  # radián / @Budapest
+longitude = 19.040235  # fok / @Budapest
+timezone = 1.0000000  # @Budapest
+
+# Teszteléshez
+# curr_time = JD(2018, 2, 14, 12, 00, False) # Helyi idő szerint
+# sun_position(curr_time, True)
